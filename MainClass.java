5b25ae629fab59c1db189c630f5ffba25ad48342
gazalchawla <gchawla@uncc.edu>
2016-02-01 03:08:01 -0500

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,71 +1,180 @@
 package eightPuzzleFinal;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-public class Function {
-	
-	//to check if some node already exists
-	public static boolean areEqual(int[][]a1, int[][]a2)
+public class MainClass {
+	 
+	//comparator for priority queue
+	public static Comparator<Node> fValueComparator = new Comparator<Node>()
+	 
 	{
-		if(a1==null)
+		@Override
+		public int compare(Node n1, Node n2)
 		{
-			return (a2==null);
+			return (int) (n1.getfVal()-n2.getfVal());
 		}
-		if(a2==null)
-		{
-			return false;
+	
+	};
+	
+	private int generatedNode;
+	private int expandedNode;
+	private PriorityQueue<Node> pq = new PriorityQueue<Node>(11,fValueComparator);
+	private Node nodeGoal = new Node();
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MainClass mainObj = new MainClass();
+		Node nodeStart = new Node();
+		nodeStart.parentNode = null;
+		int[][] current = new int[3][3];
+		System.out.println("Input the initial state.");
+		
+		current = nodeStart.inputConfiguration();
+		nodeStart.setNodeState(current);
+		
+		
+		//System.out.println(Arrays.deepToString(nodeStart.getNodeState()));
+		
+		System.out.println("Input the goal state.");
+		current = mainObj.nodeGoal.inputConfiguration();
+		mainObj.nodeGoal.setNodeState(current);
+
+		int m = nodeStart.calculateManhattanDist(nodeStart.getNodeState(), mainObj.nodeGoal.getNodeState());
+		nodeStart.sethVal(m);
+		nodeStart.setgVal(0);
+		nodeStart.setfVal(nodeStart.getgVal()+nodeStart.gethVal());
+		//System.out.println(m);
+
+		
+		mainObj.pq.add(nodeStart);
+		
+		mainObj.search();
+	}
+	
+	private void search ()
+	{	
+		//System.out.println("hey i am check");
+		Node currentNode;
+		Node goal=null;
+		int s=0;
+		
+		//when priority queue still has a node in it
+		while((currentNode=pq.poll())!=null)
+		{	
+			currentNode.generateCandidateNodes(currentNode.getNodeState());
+			
+			
+			//candidates generated and add them to priority queue only if node not equal to null
+			//if the candidate node generated is null then we dont add it to the priority queue
+			if(currentNode.getNodeLeft()!=null)
+			{
+				//pass left node to local node because hVal attribute one for one object
+				Node leftNode = new Node();
+				generatedNode++;
+				leftNode.setNodeState(currentNode.getNodeLeft());
+				leftNode.sethVal(leftNode.calculateManhattanDist(leftNode.getNodeState(), nodeGoal.getNodeState()));
+				leftNode.setgVal(currentNode.getgVal()+1);
+				leftNode.setfVal(leftNode.gethVal()+leftNode.getgVal());
+				if(leftNode.gethVal()!=0)
+				{
+					leftNode.parentNode = currentNode;
+					pq.add(leftNode);
+				}
+				
+				//for when the goal is reached
+				else
+				{
+					leftNode.parentNode = currentNode;
+					goal = leftNode;
+					s = 1;
+					break;
+					
+				}
+			}
+			
+			if(currentNode.getNodeRight()!=null)
+			{
+				Node rightNode = new Node();
+				generatedNode++;
+				rightNode.setNodeState(currentNode.getNodeRight());
+				rightNode.sethVal(rightNode.calculateManhattanDist(rightNode.getNodeState(), nodeGoal.getNodeState()));
+				rightNode.setgVal(currentNode.getgVal()+1);
+				rightNode.setfVal(rightNode.gethVal()+rightNode.getgVal());
+				if(rightNode.gethVal()!=0)
+				{
+					rightNode.parentNode = currentNode;
+					pq.add(rightNode);
+				}
+				else
+				{
+					rightNode.parentNode = currentNode;
+
+					goal = rightNode;
+					s = 1;
+					break;
+				}
+			}
+			
+			if(currentNode.getNodeUp()!=null)
+			{
+				Node upNode = new Node();
+				generatedNode++;
+				upNode.setNodeState(currentNode.getNodeUp());
+				upNode.sethVal(upNode.calculateManhattanDist(upNode.getNodeState(), nodeGoal.getNodeState()));
+				upNode.setgVal(currentNode.getgVal()+1);
+				upNode.setfVal(upNode.gethVal()+upNode.getgVal());
+				if(upNode.gethVal()!=0)
+				{
+					upNode.parentNode = currentNode;
+					pq.add(upNode);
+				}
+				else
+				{
+					upNode.parentNode = currentNode;
+					goal = upNode;
+					s = 1;
+					break;
+				}
+			}
+			
+			if(currentNode.getNodeDown()!=null)
+			{
+				Node downNode = new Node();
+				generatedNode++;
+				downNode.setNodeState(currentNode.getNodeDown());
+				downNode.sethVal(downNode.calculateManhattanDist(downNode.getNodeState(), nodeGoal.getNodeState()));
+				downNode.setgVal(currentNode.getgVal()+1);
+				downNode.setfVal(downNode.gethVal()+downNode.getgVal());
+				if(downNode.gethVal()!=0)
+				{
+					downNode.parentNode = currentNode;
+					pq.add(downNode);
+				}
+				else
+				{					
+					downNode.parentNode = currentNode;
+					goal = downNode;
+					s = 1;
+					break;		
+				}
+			}
+			expandedNode++;
 		}
 		
-		for(int i=0;i<a1.length;i++)
+		if (s==1)
 		{
-			if(!Arrays.equals(a1[i], a2[i]))
+			System.out.println("Number of generated nodes (excluding the root node): "+generatedNode);
+			System.out.println("Number of nodes expanded: "+expandedNode);
+			while(goal!=null)
 			{
-				return false;
+			Function.returnConfiguration(goal.getNodeState());			
+			goal = goal.parentNode;
 			}
 		}
-		return true;
 	}
-	
-	
-	public static void returnConfiguration(int[][] a)
-	{
-		System.out.println("Solution Path:\n");
-		for(int row=0;row<3;row++)
-		{
-			for(int col=0;col<3;col++)
-			{
-				System.out.print("\t"+a[row][col]);
-			}
-			System.out.println();
-		}
-	}
-	
-	
-	//function to swap two elements of a matrix
-	// swaps a[x][y] and a[xNew][yNew]
-	public static int[][] swapElements(int[][] arr, int x, int y, int xNew, int yNew)
-	{
-		int temp = arr[x][y];
-		arr[x][y] = arr[xNew][yNew];
-		arr[xNew][yNew] = temp;
-		return arr;
-	}
-	
-	
-	//function to copy elements of one matrix into another
-	public static int[][] copyMatrix(int[][] current)
-	{
-		int[][] temp = new int[3][3];
-		for(int row=0;row<3;row++)
-		{
-			for(int col=0;col<3;col++)
-			{
-				temp[row][col] = current[row][col];
-			}
-		}
-		return temp;
-	}
-	
-	
-	
+		
 }
